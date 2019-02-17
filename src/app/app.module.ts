507af361b32e9f0d86c05b41a4b88957c9a54eb4
Narcis Cristea <narcis.cristea@alcumusgroup.com>
2019-02-17 14:46:09 +0000

    Added weather-card and amended weather-service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { MatCardModule, MatExpansionModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WeatherPanelsComponent } from './weather-panels/weather-panels.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        WeatherCardComponent
+        WeatherPanelsComponent
     ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        HttpClientModule
+        HttpClientModule,
+        MatCardModule,
+        MatExpansionModule,
+        BrowserAnimationsModule
+
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { CityWeatherServer } from './interfaces/city-weather-server';
+import { CityWeather } from './interfaces/city-weather';
 
 @Injectable({
     providedIn: 'root'
@@ -11,19 +13,36 @@ export class WeatherService {
     public apiKey = 'a1f2ab50ae34639bcf782cdc54fcd195';
     public weatherURL = 'http://api.openweathermap.org/data/2.5/';
 
-    constructor(private http: HttpClient) {
-        this.getCityWeather('London').subscribe((data) => {
-            console.log(data);
+    constructor(private http: HttpClient) {}
+
+    public getCityWeather(city): Observable<CityWeather> {
+        return Observable.create((observer) => {
+            const queryURL = this.urlBuilder(city, 'weather');
+            this.http.get(queryURL).subscribe((data: CityWeatherServer) => {
+                const cityWeather: CityWeather = { wind: null, temperature: null, weather: null };
+                cityWeather.wind = data.wind.speed;
+                cityWeather.temperature = Math.round(data.main.temp * 10) / 10;
+                cityWeather.weather = data.weather[0].main;
+                observer.next(cityWeather);
+            });
         });
     }
 
-    public getCityWeather(city): Observable<object> {
-        const queryURL = this.urlBuilder(city, 'weather');
-        return this.http.get(queryURL);
+    public getCityForecast(city): Observable<CityWeather> {
+        return Observable.create((observer) => {
+            const queryURL = this.urlBuilder(city, 'weather');
+            this.http.get(queryURL).subscribe((data: CityWeatherServer) => {
+                const cityWeather: CityWeather = { wind: null, temperature: null, weather: null };
+                cityWeather.wind = data.wind.speed;
+                cityWeather.temperature = Math.round(data.main.temp * 10) / 10;
+                cityWeather.weather = data.weather[0].main;
+                observer.next(cityWeather);
+            });
+        });
     }
 
     private urlBuilder(city, dataType): string {
-        return this.weatherURL + dataType + '?q=' + city + '&appid=' + this.apiKey;
+        return this.weatherURL + dataType + '?q=' + city + '&appid=' + this.apiKey + '&units=metric';
     }
 
 }
